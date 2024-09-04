7874f479ce072e9291933e4db29e246831637ef7
yan0606 <yan060604@gmail.com>
2024-09-04 11:17:30 -0300

    feat :sparkles: relatorio de todos os produtos para o perfil de ADM

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ let url_initial = "http://localhost/E-commerceAPI-PHPpure/";
 function enviarFormulario() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    // Fazer verificação dos campos
+
     const data = {
         email: email,
         password: password
@@ -17,14 +17,19 @@ function enviarFormulario() {
         dataType: "JSON",
     }).done(async function (response) {
         alert(response.message);
-        localStorage.setItem("token", response.token); // adiciona token ao banco de dados do navegador
-        localStorage.setItem("id", response.id); // adiciona token ao banco de dados do navegador
-        const token = localStorage.getItem("token"); //pega o token do banco de dados do navegador
-        console.log("TOKEN NO LOCAL STORAGE =>" + token); //exibe o token no console, só pra conferir :)
-        window.location.href = "../index.html";
+        localStorage.setItem("token", response.token); // adiciona token ao localStorage
+        localStorage.setItem("id", response.id); // adiciona id ao localStorage
+        const token = localStorage.getItem("token"); // pega o token do localStorage
+        console.log("TOKEN NO LOCAL STORAGE =>" + token); // exibe o token no console, só pra conferir :)
+
+        // Verifica se o login é do administrador
+        if (email === "yanADM@gmail.com" && password === "12345678") {
+            window.location.href = "../pages/admin.html"; // redireciona para a página de administrador
+        } else {
+            window.location.href = "../index.html"; // redireciona para a página normal
+        }
+
     }).fail(function (error) {
         alert(error.responseJSON.message);
-    })
-
-
+    });
 }
